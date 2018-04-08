67440c6b7c6a1f2c0cc927dec24a8cf18def4e18
lxh <lixiaohua@itcast.cn>
2018-04-08 19:08:54 +0800

    A

--- src/main/java/cn/itcast/demo/Test.java
+++ src/main/java/cn/itcast/demo/Test.java
@@ -12,6 +12,7 @@ public class Test {
 		System.out.println("========1111========");
 		System.out.println("========2222=======");
 		System.out.println("========3333=======");
+		System.out.println("========4444=======");
 	}
 
 }
